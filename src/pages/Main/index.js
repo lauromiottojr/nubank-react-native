42cdd5340c0538e23dfd7c343db48673fa23c25d
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-05-02 20:52:32 -0300

    Configure Card and Tabs

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666"/>
+            <Icon name="visibility-off" size={28} color="#666"/>
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$197.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$20,00 recebida de Laurão hoje às 06:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+      <Tabs />
     </Container>
   );
 }
