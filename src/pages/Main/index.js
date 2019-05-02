b3730af294f7d6604d794e81b4304ca414a6b5c7
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-05-02 20:13:40 -0300

    Configure Icon and Image

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import { Container } from './styles';
 
+import Header from '~/components/Header';
+
 export default function Main() {
   return (
-    <Container />
+    <Container>
+      <Header />
+    </Container>
   );
 }
