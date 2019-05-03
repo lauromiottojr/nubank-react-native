f443527ee9331ce939749bdaca49549b2c50c60e
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-05-02 21:58:24 -0300

    Adding PanGestureHandler and Configure

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
 import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 import Header from '~/components/Header';
@@ -9,26 +12,54 @@ import Tabs from '~/components/Tabs';
 import Menu from '~/components/Menu';
 
 export default function Main() {
+
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = new Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        }
+      }
+    ],
+    { useNativeDriver: true }
+  );
+
+  function onHandlerStateChanged(event) {
+
+  }
+
   return (
     <Container>
       <Header />
       <Content>
         <Menu />
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$197.611,65</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$20,00 recebida de Laurão hoje às 06:00h
+        <PanGestureHandler onGestureEvent={animatedEvent} onHandlerStateChange={onHandlerStateChanged}>
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [0, 380], // se o card estiver fora desses valorres px, 
+                outputRange: [0, 380],
+                extrapolate: 'clamp', // esse atributo não deixa ele proceguir
+              }),
+            }]
+          }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$197.611,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$20,00 recebida de Laurão hoje às 06:00h
             </Annotation>
-          </CardFooter>
-        </Card>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
       <Tabs />
     </Container>
