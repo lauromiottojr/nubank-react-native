235ea685f63a083082fdb04b7b0a1b825b265788
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-05-02 22:24:24 -0300

    Improving Animated in Tabs

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -7,6 +7,13 @@ import { Container, TabsContainer, TabItem, TabText } from './styles';
 export default function Tabs({ translateY }) {
     return (
         <Container style={{
+            transform: [{
+                translateY: translateY.interpolate({
+                    inputRange: [0, 380],
+                    outputRange: [0, 30],
+                    extrapolate: 'clamp',
+                }),
+            }],
             opacity: translateY.interpolate({
                 inputRange: [0, 380],
                 outputRange: [1, 0.3],
