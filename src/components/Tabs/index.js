af32c69fef015a7f8281111df2b2e6238db3f3dc
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-05-02 22:21:26 -0300

    Adding Animated in Tabs

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -4,28 +4,35 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, TabsContainer, TabItem, TabText } from './styles';
 
-export default function Tabs() {
+export default function Tabs({ translateY }) {
     return (
-        <Container>
+        <Container style={{
+            opacity: translateY.interpolate({
+                inputRange: [0, 380],
+                outputRange: [1, 0.3],
+                extrapolate: 'clamp',
+            }),
+        }}
+        >
             <TabsContainer>
                 <TabItem>
-                    <Icon name="person-add" size={24} color="#FFF"/>
+                    <Icon name="person-add" size={24} color="#FFF" />
                     <TabText>Indicar amigos</TabText>
                 </TabItem>
                 <TabItem>
-                    <Icon name="chat-bubble-outline" size={24} color="#FFF"/>
+                    <Icon name="chat-bubble-outline" size={24} color="#FFF" />
                     <TabText>Cobrar</TabText>
                 </TabItem>
                 <TabItem>
-                    <Icon name="arrow-downward" size={24} color="#FFF"/>
+                    <Icon name="arrow-downward" size={24} color="#FFF" />
                     <TabText>Depositar</TabText>
                 </TabItem>
                 <TabItem>
-                    <Icon name="arrow-upward" size={24} color="#FFF"/>
+                    <Icon name="arrow-upward" size={24} color="#FFF" />
                     <TabText>Transferir</TabText>
                 </TabItem>
                 <TabItem>
-                    <Icon name="lock" size={24} color="#FFF"/>
+                    <Icon name="lock" size={24} color="#FFF" />
                     <TabText>Bloquear cartão</TabText>
                 </TabItem>
             </TabsContainer>
